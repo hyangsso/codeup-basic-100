e46aeeb8286f513c147f76c0fb59122e514edb4f
hyangsso <sohyang8946@gmail.com>
2023-06-05 14:37:25 +0900

    61~80번까지의 문제풀이 완료

---
+++ training/6069.py
@@ -0,0 +1,11 @@
+text = input()
+if text == "A":
+    print("best!!!")
+elif text == "B":
+    print("good!!")
+elif text == "C":
+    print("run!")
+elif text == "D":
+    print("slowly~")
+else:
+    print("what?")
---
+++ training/6074.py
@@ -0,0 +1,7 @@
+c = ord(input())
+t = ord('a')
+while t<=c :
+    print(chr(t), end=' ')
+    t += 1
+    
+# 알파벳 문자 a의 정수값은 ord('a')로 알아낼 수 있음
