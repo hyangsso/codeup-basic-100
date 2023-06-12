b6c90d93f0d324824f60f83774eb189662fabdca
hyangsso <sohyang8946@gmail.com>
2023-06-12 15:38:36 +0900

    feat: 81번부터 96번까지 문제풀이

---
+++ training/6088.py
@@ -0,0 +1,5 @@
+a, d, n = input().split()
+a, d, n = int(a), int(d), int(n)
+
+x = a + d*(n-1)
+print(x)
---
+++ training/6089.py
@@ -0,0 +1,5 @@
+a, r, n = input().split()
+a, r, n = int(a), int(r), int(n)
+
+x = a*(r**(n-1))
+print(x)
